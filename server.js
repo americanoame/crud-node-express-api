c3855f399759f46b09a934a2913e3967bb58da8e
Americano.Dev <andrenations@gmail.com>
2023-08-08 17:09:03 -0400

    inicial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+
+// routes
+
+app.get('/', (req, res)  => {
+    res.send('hello Node Api')
+})
+ 
+
+app.listen(3000, ()=> {
+    console.log('Node Api is running on port 3000')
+})
