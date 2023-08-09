743d3315f8754ad6e07912633e86e071c39f6af1
Americano.Dev <andrenations@gmail.com>
2023-08-09 19:04:50 -0400

    create model for database and save the data to mongodb

--- models/productModels.js
+++ models/productModels.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose(
+const productSchema = mongoose.Schema(
   {
     name: {
       type: String,
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-const mongoose = require('mongoose');
-
 const express = require('express');
-
+const mongoose = require('mongoose');
+const Product = require('./models/productModels')
 const app = express();
 
+app.use(express.json())
 
+
+
+// routes
+
+app.get('/blog', (req, res)  => {
+    res.send('hello Node Api')
+})
+
+app.post('/product', async(req, res)  => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose.connect('mongodb+srv://andrenations:TUNdraLF49UVrbu6@cluster0.rlurztn.mongodb.net/?retryWrites=true&w=majority')
 
@@ -16,17 +34,6 @@ mongoose.connect('mongodb+srv://andrenations:TUNdraLF49UVrbu6@cluster0.rlurztn.m
 });
 
 
-
-
-// routes
-
-app.get('/', (req, res)  => {
-    res.send('hello Node Api')
-})
-
-app.get('/this', (req, res)  => {
-    res.send('this is it')
-})
  
 
 app.listen(3000, ()=> {
@@ -35,6 +42,3 @@ app.listen(3000, ()=> {
 
 
 
-// TUNdraLF49UVrbu6
-
-// mongodb+srv://andrenations:TUNdraLF49UVrbu6@cluster0.rlurztn.mongodb.net/crud?retryWrites=true&w=majority
