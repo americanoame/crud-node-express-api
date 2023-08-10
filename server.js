d1c90a0354d876ac6d08a57b96c0211a051432c1
Americano.Dev <andrenations@gmail.com>
2023-08-09 23:22:14 -0400

    created routes and tested it with insomnia

--- server.js
+++ server.js
@@ -13,6 +13,37 @@ app.get('/blog', (req, res)  => {
     res.send('hello Node Api')
 })
 
+// 2part getting data from the database
+
+app.get('/products', async(req, res) => {
+    try {
+         const products = await Product.find({});
+         res.status(200).json(products);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+})
+
+// 3part find products by :id 
+
+app.get('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+         const product = await Product.findById(id);
+         res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+})
+
+
+
+// 1part creating data in the database 
+
 app.post('/product', async(req, res)  => {
     try {
         const product = await Product.create(req.body)
@@ -23,6 +54,42 @@ app.post('/product', async(req, res)  => {
         res.status(500).json({message: error.message})
     }
 })
+
+
+
+// update a product 
+
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product) {
+            return res.status(404).json({message: `there's no product wit ID ${id}`})
+        }
+        const updateProduct = await Product.findById(id);
+        res.status(200).json(updateProduct);
+
+    } catch (error) {
+      res.status(500).json({message: error.message})  
+    }
+})
+
+// delete a product
+
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product) {
+            return res.status(404).json({message: `there's no product wit ID ${id}`})
+        }
+       res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 
 mongoose.connect('mongodb+srv://andrenations:TUNdraLF49UVrbu6@cluster0.rlurztn.mongodb.net/?retryWrites=true&w=majority')
 
